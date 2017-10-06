81eb9b52170ab599b8a703acb16d847448a2720b
@ykhanal <ykhanal@ztr.com>
2017-10-05 21:35:32 -0400

    make sure commit and push is ok

--- app.js
+++ app.js
@@ -2,24 +2,49 @@
 	'use strict';
 
 	angular.module('ShoppingList', []).controller('ToBuyController',
-			ToBuyController)
-			.controller('BoughtController', BoughtController)
-			.service('ShoppingService',ShoppingService);
+			ToBuyController).controller('BoughtController', BoughtController)
+			.service('ShoppingService', ShoppingService);
 
-	ToBuyController.$inject = [ '$scope', 'ShoppingService' ];
-	function ToBuyController($scope, ShoppingService) {
+	ToBuyController.$inject = [ 'ShoppingService' ];
+	function ToBuyController(ShoppingService) {
 		var vm = this;
-		
+		vm.getToBuyItems = ShoppingService.getToBuyItems();
+		vm.buy = function(index){
+			ShoppingService.removeItem(index);
+			
+		}
+
+
 	}
-	
-	BoughtController.$inject = ['$scope',"ShoppingService"];
-	function BoughtController($scope, ShoopingService){
+
+	BoughtController.$inject = [ 'ShoppingService' ];
+	function BoughtController(ShoppingService) {
 		var vm = this;
+		vm.getBoughtItems = ShoppingService.getBoughtItems();
+
 	}
-	
-	ShoppingService.$inject = ['$scope'];
-	function ShoppingService($scope){
+
+	// ShoppingService.$inject = ['$scope'];
+	function ShoppingService() {
 		var service = this;
+		var toBuy = [];
+		var bought = [];
+		service.getToBuyItems = function() {
+			toBuy.push({qty:10, text:'cookies'});
+			toBuy.push({qty:8, text:'chocoloate'});
+			toBuy.push({qty:7, text:'candy'});
+			toBuy.push({qty:10, text:'donuts'});
+			toBuy.push({qty:11, text:'apple'});
+			return toBuy;
+		}
+
+		service.getBoughtItems = function(){
+			return bought;
+		};
+		service.removeItem = function(index){
+			bought.push(toBuy[index]);
+			toBuy.splice(index,1);
+		};
 		
 	}
 
